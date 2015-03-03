8f9b8ed05ee9132f7ca1a23a35ccb8a3027d2159
ZHONGHuanGit <wpzhonghuan@gmail.com>
2015-03-03 12:31:17 +0800

    Initial Commit

---
+++ ZHShowDesktop/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ZHShowDesktop
+//
+//  Created by 钟桓 on 15/3/3.
+//  Copyright (c) 2015年 钟桓. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
