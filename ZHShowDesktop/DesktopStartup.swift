dea3e2150ccc9c72626ddaf62ccc8101097f7f58
ZHONGHuanGit <wpzhonghuan@gmail.com>
2015-03-03 21:38:30 +0800

    show desktop

---
+++ ZHShowDesktop/DesktopStartup.swift
@@ -0,0 +1,9 @@
+//
+//  DesktopLogin.swift
+//  ZHShowDesktop
+//
+//  Created by 钟桓 on 15/3/3.
+//  Copyright (c) 2015年 钟桓. All rights reserved.
+//
+
+import Foundation
---
+++ ZHShowDesktop/SimulateKeyBoardEvent.swift
@@ -0,0 +1,9 @@
+//
+//  SimulateKeyBoardEvent.swift
+//  ZHShowDesktop
+//
+//  Created by 钟桓 on 15/3/3.
+//  Copyright (c) 2015年 钟桓. All rights reserved.
+//
+
+import Foundation
